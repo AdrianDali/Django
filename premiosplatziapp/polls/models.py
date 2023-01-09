9fc0a7c1dd3fd341062b7154faffe82209606ea5
AdrianDali <hadriandali@gmail.com>
2023-01-09 01:56:16 -0600

    Back

--- premiosplatziapp/polls/models.py
+++ premiosplatziapp/polls/models.py
@@ -1,3 +1,24 @@
+import datetime 
 from django.db import models
-
+from django.utils import timezone 
 # Create your models here.
+
+class Question(models.Model):
+    #no es necesario poner el id, ya que se crea automaticamente
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        #time delta es un objeto que mide la diferencia entre dos fechas
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,8 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question,Choice
+from django.urls import reverse 
+#las vistas siempre reciben un request y devuelven un HttpResponse 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.all()
+
+    return render(request, "polls/index.html", {"latest_question_list": latest_question_list})
     
-# Create your views here.
+def detail(request, question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+def results(request, question_id):
+    return HttpResponse("Estas viendo los resultados de la pregunta numero %s." % question_id)
+
+def vote(request,  question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try: 
+        #intentamos obtener la opcion seleccionada
+        selected_choice = question.choice_set.get(pk = request.POST["choice"])
+
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "No seleccionaste una opcion",
+        })
+    #si todo salio bien
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
